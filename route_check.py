80da4fae8da43acda74adbfea3818d70ab4b8b14
rdarbha <rama.h.darbha@gmail.com>
2017-03-06 18:20:01 -0500

    debugging testing script

--- route_check.py
+++ route_check.py
@@ -26,15 +26,12 @@ for command in commands:
   # print parsed_json[0][0]
 
   for node in parsed_json[0]:
-    #print node['reason']
-    print node['ip']
+    print "Current route check:" + node['ip']
     if str(node['ip']) in check_routes:
-      # print "Found The Route"
-      # print "before"
-      # print check_routes
+      print "Found The Route"
+      print "before" + check_routes
       check_routes.remove(node['ip'])
-      # print "after"
-      # print check_routes
+      print "after" + check_routes
     else:
       lost_routes.append(str(node['ip']))
 
