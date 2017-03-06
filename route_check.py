55ba59a7fc5380394661f665b0ce06b22ab81531
rdarbha <rama.h.darbha@gmail.com>
2017-03-06 18:00:51 -0500

    adding route script

--- route_check.py
+++ route_check.py
@@ -10,27 +10,28 @@ import time
 commands = ["netq show ip route leaf01 json"]
 failed_nodes = []
 
-current_routes = ["10.254.0.2","10.254.0.5","10.254.0.5","10.254.0.6","10.254.0.7"]
-new_routes = []
+check_routes = ["10.254.0.2","10.254.0.5","10.254.0.5","10.254.0.6","10.254.0.7"]
+found_routes = []
+lost_routes = []
 
 for command in commands:
   # call(["ls", "-l"])
   result = subprocess.check_output(command, shell=True)
-  print result
+  # print result
   parsed_json =  json.loads(result)
-  print parsed_json
-  print
-  print parsed_json[0]
-  print
-  print parsed_json[0][0]
+  # print parsed_json
+  # print
+  # print parsed_json[0]
+  # print
+  # print parsed_json[0][0]
 
   for node in parsed_json[0]:
     #print node['reason']
     print node['ip']
-    if node['ip'] in current_routes:
+    if str(node['ip']) in current_routes:
       current_routes.remove(node['ip'])
     else:
-      new_routes.append(node['ip'])
+      lost_routes.append(str(node['ip'])
 
 print current_routes
-print new_routes
+print lost_routes
