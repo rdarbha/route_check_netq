e3f03bb23c04c385a0ed53b8aaf3d82a24c992e0
rdarbha <rama.h.darbha@gmail.com>
2017-03-06 22:03:41 -0500

    Updating to read from file

--- route_check.py
+++ route_check.py
@@ -2,21 +2,42 @@ import redis
 import subprocess
 import json
 import time
+import docopt
+
+"""Store and check route statements.
+
+Usage:
+  route_check.py store <node>
+  route_check.py check <node>
+
+--store   Take the current route table and store it
+--check   Compare the current route table against the stored version
+"""
+
+arguments = docopt.docopt(__doc__)
 
 commands = ["netq show ip route leaf01 json"]
 
+#Commented out because reading in routes from file now
 # stored_routes = ["10.254.0.2/32","10.254.0.3/32","10.254.0.4/32","10.254.0.5/32","10.254.0.6/32","10.254.0.7/32","10.254.0.8/32"]
+
 found_routes = []
 lost_routes = []
 
+# Read in file. Each line is a route statement
 fname = "route_list"
 
-with open(fname) as f:
-  stored_routes = f.readlines()
+if arguments['check']
+  with open(fname) as f:
+    stored_routes = f.readlines()
 
-stored_routes = [x.strip() for x in stored_routes]
+  stored_routes = [x.strip() for x in stored_routes]
 
-print stored_routes
+# if arguments['store']
+# with open(fname) as f:
+#   stored_routes = f.write()
+
+# print stored_routes
 
 for command in commands:
   result = subprocess.check_output(command, shell=True)
