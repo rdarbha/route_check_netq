d5fa34314fe601ccab28bb1538cfa039fd6be06b
rdarbha <rama.h.darbha@gmail.com>
2017-03-06 22:31:56 -0500

    Adding docopt

--- route_check.py
+++ route_check.py
@@ -19,9 +19,9 @@ import docopt
 arguments = docopt.docopt(__doc__)
 
 # commands = ["netq show ip route leaf01 json"]
-commands = ["netq show ip route " + arguments['<node>'] + " json"]
+command = "netq show ip route " + arguments['<node>'] + " json"
+# commands = ["netq show ip route " + arguments['<node>'] + " json"]
 
-print commands
 #Commented out because reading in routes from file now
 # stored_routes = ["10.254.0.2/32","10.254.0.3/32","10.254.0.4/32","10.254.0.5/32","10.254.0.6/32","10.254.0.7/32","10.254.0.8/32"]
 
@@ -29,13 +29,18 @@ found_routes = []
 lost_routes = []
 
 # Read in file. Each line is a route statement
-fname = "route_list"
+fname = arguments['<node>']
+
+f = open(fname)
 
 if arguments['check']:
-  with open(fname) as f:
-    stored_routes = f.readlines()
-
+  stored_routes = f.readlines()
   stored_routes = [x.strip() for x in stored_routes]
+  command_output = GetCommandOutput(command)
+  CheckRoutes(command_output)
+
+if arguments['store']:
+  print "Implement store functionality"
 
 # if arguments['store']
 # with open(fname) as f:
@@ -43,25 +48,36 @@ if arguments['check']:
 
 # print stored_routes
 
-for command in commands:
+def GetCommandOutput(command):
+
   result = subprocess.check_output(command, shell=True)
-  # print result
-  parsed_json =  json.loads(result)
-  # print parsed_json
-  # print
-  # print parsed_json[0]
-  # print
-  # print parsed_json[0][0]
+  parsed_json = json.loads(result)
 
-  for node in parsed_json[0]:
-    # print "Current route check:" + node['ip']
-    if str(node['ip']) in stored_routes:
-      # print "Found The Route"
-      # print "before: " + str(check_routes)
-      stored_routes.remove(node['ip'])
-      # print "after: " + str(check_routes)
-    else:
-      lost_routes.append(str(node['ip']))
+  #This was the way to run multiple commands using a list of commands
+  # for command in commands:
+  #   result = subprocess.check_output(command, shell=True)
+  #   # print result
+  #   parsed_json =  json.loads(result)
+    # print parsed_json
+    # print
+    # print parsed_json[0]
+    # print
+    # print parsed_json[0][0]
 
-print "Routes not in current routing table: " + str(stored_routes)
-print "Routes unexpectedly found in routing table: " + str(lost_routes)
+  #Need to return parsed_json[0] because netq nests json in two arrays
+  return parsed_json[0]
+
+def CheckRoutes(parsed_json):
+
+    for node in parsed_json[0]:
+      # print "Current route check:" + node['ip']
+      if str(node['ip']) in stored_routes:
+        # print "Found The Route"
+        # print "before: " + str(check_routes)
+        stored_routes.remove(node['ip'])
+        # print "after: " + str(check_routes)
+      else:
+        lost_routes.append(str(node['ip']))
+
+  print "Routes not in current routing table: " + str(stored_routes)
+  print "Routes unexpectedly found in routing table: " + str(lost_routes)
