85be9aa48dcced4dccadf11b48ad54e48571cae9
rdarbha <rama.h.darbha@gmail.com>
2017-03-06 22:33:17 -0500

    Adding docopt

--- route_check.py
+++ route_check.py
@@ -37,7 +37,7 @@ if arguments['check']:
   stored_routes = f.readlines()
   stored_routes = [x.strip() for x in stored_routes]
   command_output = GetCommandOutput(command)
-  CheckRoutes(command_output)
+  CheckRoutes(command_output, stored_routes)
 
 if arguments['store']:
   print "Implement store functionality"
@@ -67,17 +67,20 @@ def GetCommandOutput(command):
   #Need to return parsed_json[0] because netq nests json in two arrays
   return parsed_json[0]
 
-def CheckRoutes(parsed_json):
+def CheckRoutes(parsed_json, stored_routes):
 
-    for node in parsed_json[0]:
-      # print "Current route check:" + node['ip']
-      if str(node['ip']) in stored_routes:
-        # print "Found The Route"
-        # print "before: " + str(check_routes)
-        stored_routes.remove(node['ip'])
-        # print "after: " + str(check_routes)
-      else:
-        lost_routes.append(str(node['ip']))
+  found_routes = []
+  lost_routes = []
+
+  for node in parsed_json[0]:
+    # print "Current route check:" + node['ip']
+    if str(node['ip']) in stored_routes:
+      # print "Found The Route"
+      # print "before: " + str(check_routes)
+      stored_routes.remove(node['ip'])
+      # print "after: " + str(check_routes)
+    else:
+      lost_routes.append(str(node['ip']))
 
   print "Routes not in current routing table: " + str(stored_routes)
   print "Routes unexpectedly found in routing table: " + str(lost_routes)
