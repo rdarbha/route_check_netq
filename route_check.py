71d952ef93183098a390e62075a5ae3dca4d6b7d
rdarbha <rama.h.darbha@gmail.com>
2017-03-06 22:38:18 -0500

    Adding docopt

--- route_check.py
+++ route_check.py
@@ -16,31 +16,6 @@ import json
 import time
 import docopt
 
-if __name__ == '__main__':
-  arguments = docopt.docopt(__doc__)
-
-  command = "netq show ip route " + arguments['<node>'] + " json"
-
-  #Commented out because reading in routes from file now
-  # stored_routes = ["10.254.0.2/32","10.254.0.3/32","10.254.0.4/32","10.254.0.5/32","10.254.0.6/32","10.254.0.7/32","10.254.0.8/32"]
-
-  found_routes = []
-  lost_routes = []
-
-  # Read in file. Each line is a route statement
-  fname = arguments['<node>']
-
-  f = open(fname)
-
-  if arguments['check']:
-    stored_routes = f.readlines()
-    stored_routes = [x.strip() for x in stored_routes]
-    command_output = GetCommandOutput(command)
-    CheckRoutes(command_output, stored_routes)
-
-  if arguments['store']:
-    print "Implement store functionality"
-
 def GetCommandOutput(command):
 
   result = subprocess.check_output(command, shell=True)
@@ -66,3 +41,28 @@ def CheckRoutes(parsed_json, stored_routes):
 
   print "Routes not in current routing table: " + str(stored_routes)
   print "Routes unexpectedly found in routing table: " + str(lost_routes)
+
+if __name__ == '__main__':
+  arguments = docopt.docopt(__doc__)
+
+  command = "netq show ip route " + arguments['<node>'] + " json"
+
+  #Commented out because reading in routes from file now
+  # stored_routes = ["10.254.0.2/32","10.254.0.3/32","10.254.0.4/32","10.254.0.5/32","10.254.0.6/32","10.254.0.7/32","10.254.0.8/32"]
+
+  found_routes = []
+  lost_routes = []
+
+  # Read in file. Each line is a route statement
+  fname = arguments['<node>']
+
+  f = open(fname)
+
+  if arguments['check']:
+    stored_routes = f.readlines()
+    stored_routes = [x.strip() for x in stored_routes]
+    command_output = GetCommandOutput(command)
+    CheckRoutes(command_output, stored_routes)
+
+  if arguments['store']:
+    print "Implement store functionality"
