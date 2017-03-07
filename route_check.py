5b27752eddd237391f367dd03aa790c870bc37a3
rdarbha <rama.h.darbha@gmail.com>
2017-03-06 21:35:02 -0500

    Updating to read from file

--- route_check.py
+++ route_check.py
@@ -3,19 +3,20 @@ import subprocess
 import json
 import time
 
-# redis_db = redis.StrictRedis(host="localhost", port=6379, db=0)
-# print redis_db.keys()
-
-# commands = ["ls -l","ls"]
 commands = ["netq show ip route leaf01 json"]
-failed_nodes = []
 
-check_routes = ["10.254.0.2/32","10.254.0.3/32","10.254.0.4/32","10.254.0.5/32","10.254.0.6/32","10.254.0.7/32","10.254.0.8/32"]
+# stored_routes = ["10.254.0.2/32","10.254.0.3/32","10.254.0.4/32","10.254.0.5/32","10.254.0.6/32","10.254.0.7/32","10.254.0.8/32"]
 found_routes = []
 lost_routes = []
 
+with open(fname) as f:
+  stored_routes = f.readlines()
+
+stored_routes = [x.strip() for x in stored_routes]
+
+print stored_routes
+
 for command in commands:
-  # call(["ls", "-l"])
   result = subprocess.check_output(command, shell=True)
   # print result
   parsed_json =  json.loads(result)
@@ -30,10 +31,10 @@ for command in commands:
     if str(node['ip']) in check_routes:
       # print "Found The Route"
       # print "before: " + str(check_routes)
-      check_routes.remove(node['ip'])
+      stored_routes.remove(node['ip'])
       # print "after: " + str(check_routes)
     else:
       lost_routes.append(str(node['ip']))
 
-print "Routes not in current routing table: " + str(check_routes)
+print "Routes not in current routing table: " + str(stored_routes)
 print "Routes unexpectedly found in routing table: " + str(lost_routes)
